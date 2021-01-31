97c53ad0a95974b110ac8fbf4c7648c161e03a73
Louis Young <me@louisyoung.co.uk>
2021-01-31 19:07:43 +0000

    Style user

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -19,31 +19,45 @@ const User = () => {
     company,
     location,
     blog: website,
+    name,
   } = user;
 
-  return (
-    <article className="user">
-      <img src={image} alt={username} />
-      <p>Username: {username}</p>
-      <p>Link: {link}</p>
+  console.log(user);
 
-      {description && <p>Description: {description}</p>}
+  return (
+    <article className="user card">
+      <a href={link} target="_blank" rel="noopener nofollow noreferrer" className="user__user">
+        <figure>
+          <img src={image} alt={username} className="user__photo" />
+        </figure>
+        <figcaption className="user__details">
+          <h5 className="user__name">{name}</h5>
+          <p className="user__username">{username}</p>
+        </figcaption>
+      </a>
+
+      <p className="user__description">{description}</p>
 
       {company && (
-        <p>
-          <img className="user__icon" src={office} alt={company} /> {company}
+        <p className="user__detail">
+          <img className="user__icon" src={office} alt={company} />
+          <p className="user__text">{company}</p>
         </p>
       )}
 
       {location && (
-        <p>
-          <img className="user__icon" src={pin} alt={location} /> {location}
+        <p className="user__detail">
+          <img className="user__icon" src={pin} alt={location} />
+          <p className="user__text">{location}</p>
         </p>
       )}
 
       {website && (
-        <p>
-          <img className="user__icon" src={external} alt={website} /> {website}
+        <p className="user__detail">
+          <img className="user__icon" src={external} alt={website} />
+          <a href={link} target="_blank" rel="noopener nofollow noreferrer" className="user__text link">
+            {website}
+          </a>
         </p>
       )}
     </article>
